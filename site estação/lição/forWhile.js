901afaeae2f55affb533e2ccc0642ee6957e20ac
vihitcok <42897694+vihitcok@users.noreply.github.com>
2018-09-02 18:14:13 -0300

    Add files via upload

---
+++ site estação/lição/forWhile.js
@@ -0,0 +1,35 @@
+let: contador = 0;
+undefined;
+
+while (contador < 10) {
+    contador contador ++;
+    console.log(contador);
+}
+
+let: contador = 0;
+undefined;
+
+while (contador < 10) {
+    contador contador ++;
+    console.log(contador);
+}
+
+
+while (contador < 1000) {
+    if (contador % 2 == 0) {
+    console.log(contador + "é par")
+}else{
+    console.log("é impar")
+}
+contador++;  
+}
+
+
+
+contador = 0;
+do{
+    console.log(contador);
+    contador++;
+}while (contador < 10) {
+    
+}
---
+++ site estação/script.js
@@ -0,0 +1,7 @@
+let cadastro = document.querySelector(".cadastrar");
+undefined;
+cadastro.addEventListener('click', function(){
+	prompt("digite seu email:")
+	prompt("digite seu usuário:")
+	prompt("digite sua senha:")
+	alert("Usuário cadastrado com sucesso!");})
